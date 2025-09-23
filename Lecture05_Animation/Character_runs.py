bc826312809d404caee43639cd6bcd77f4e0c9a6
GODhanQ <hg.min2018@gmail.com>
2025-09-23 18:10:08 +0900

    기존 출력엔 클리핑 위치가 왼쪽으로 밀려 다음 이미지가 출력됐음. -> 클리핑 위치 수정 하지만 5번 이미지 부턴 너비가 달라짐

--- Lecture05_Animation/Character_runs.py
+++ Lecture05_Animation/Character_runs.py
@@ -5,9 +5,9 @@ open_canvas()
 
 animation_count: Final = 4
 animation_frame_count = [10, 12, 6, 9]  # 첫 번째 애니메이션 프레임 수를 10으로 조정
-animation_frame_wANDh = [(24, 45), (24, 48), (24, 48), (24, 48), (24, 48),
-                         (24, 48), (24, 48), (24, 48), (24, 48), (24, 48),
-                         (24, 48)]  # 가로 너비를 기존 값에서 5 늘림
+animation_frame_wANDh = [(27, 45), (27, 48), (27, 48), (27, 48), (27, 48),
+                         (27, 48), (27, 48), (27, 48), (27, 48), (27, 48),
+                         (27, 48)]  # 가로 너비를 기존 값에서 5 늘림
 sprite_width = 399
 sprite_height = 525
 character = load_image("sonic-sprite.png")
@@ -15,23 +15,23 @@ character = load_image("sonic-sprite.png")
 clear_canvas()
 
 frame = 0
-bottom = sprite_height - (2 * animation_frame_wANDh[0][1]) + 10
-left = frame * animation_frame_wANDh[0][0] + 5  # left 값을 5만큼 오른쪽으로 이동
-character.clip_draw(left, bottom, animation_frame_wANDh[0][0],
-                    animation_frame_wANDh[0][1], 400, 300)
-update_canvas()
+# bottom = sprite_height - (2 * animation_frame_wANDh[0][1]) + 10
+# left = frame * animation_frame_wANDh[0][0] + 5  # left 값을 5만큼 오른쪽으로 이동
+# character.clip_draw(left, bottom, animation_frame_wANDh[0][0],
+#                     animation_frame_wANDh[0][1], 400, 300)
+# update_canvas()
 
-# while True:
-#     clear_canvas()
-#     # bottom 좌표를 약간 위로 조정
-#     bottom = sprite_height - (2 * animation_frame_wANDh[0][1]) + 10
-#     left = frame * animation_frame_wANDh[0][0] + 5  # left 값을 5만큼 오른쪽으로 이동
-#     character.clip_draw(left, bottom, animation_frame_wANDh[0][0],
-#                         animation_frame_wANDh[0][1], 410, 300)
-#     update_canvas()
-#     frame = (frame + 1) % animation_frame_count[0]
-#     delay(0.5)
-
-# close_canvas()
 while True:
-    pass
+    clear_canvas()
+
+    bottom = sprite_height - (2 * animation_frame_wANDh[0][1]) + 10
+    left = frame * animation_frame_wANDh[0][0] + 5
+
+    character.clip_draw(left, bottom, animation_frame_wANDh[0][0],
+                        animation_frame_wANDh[0][1], 410, 300)
+    update_canvas()
+    frame = (frame + 1) % animation_frame_count[0]
+    delay(0.5)
+    print(frame)
+
+close_canvas()
