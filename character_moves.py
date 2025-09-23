22d90612a0ba8237fe70fae6f9d5f850dead84b1
GODhanQ <hg.min2018@gmail.com>
2025-09-23 17:33:18 +0900

    파일 분리
    소닉 스프레드 시트에서 일부분을 순차적으로 애니메이션하기

--- character_moves.py
+++ character_moves.py
@@ -1,20 +1,13 @@
 from pico2d import *
-import time
 
 open_canvas()
+grass = load_image('Lecture05_Animation\\grass.png')
+character = load_image('Lecture05_Animation\\run_animation.png')
 
-# aniamtion_count = 4
-grass = load_image("Lecture05_Animation\\grass.png")
-character = load_image("Lecture05_Animation\\run_animation.png")
-
-start = time.time()
 frame = 0
 for x in range(0, 800, 10):
-    # duration = start - time.time()
-    # if (duration >= 5.0):
-    #     animation_mod = (animation_mod + 1) % aniamtion_count
     clear_canvas()
-    grass.draw(400,30)
+    grass.draw(400, 30)
     character.clip_draw(frame * 100, 0, 100, 100, x, 90)
     update_canvas()
     frame = (frame + 1) % 8
