537128b2f25b861ca2414e607dc2c5a69b8e7b7b
GODhanQ <hg.min2018@gmail.com>
2025-09-23 18:14:04 +0900

    이미지가 이동하는 것 처럼 보이진 않아졌지만, 옆 이미지가 조금 출력됨

--- Lecture05_Animation/Character_runs.py
+++ Lecture05_Animation/Character_runs.py
@@ -4,28 +4,24 @@ from typing import Final
 open_canvas()
 
 animation_count: Final = 4
-animation_frame_count = [10, 12, 6, 9]  # 첫 번째 애니메이션 프레임 수를 10으로 조정
-animation_frame_wANDh = [(27, 45), (27, 48), (27, 48), (27, 48), (27, 48),
-                         (27, 48), (27, 48), (27, 48), (27, 48), (27, 48),
-                         (27, 48)]  # 가로 너비를 기존 값에서 5 늘림
+animation_frame_count = [10, 12, 6, 9]
+animation_frame_wANDh = [(27, 45), (27, 48), (27, 48), (27, 48), (32, 48),
+                         (32, 48), (32, 48), (32, 48), (32, 48), (32, 48),
+                         (32, 48)]  # 가로 너비를 기존 값에서 3 더 늘림
 sprite_width = 399
 sprite_height = 525
 character = load_image("sonic-sprite.png")
 
-clear_canvas()
-
 frame = 0
-# bottom = sprite_height - (2 * animation_frame_wANDh[0][1]) + 10
-# left = frame * animation_frame_wANDh[0][0] + 5  # left 값을 5만큼 오른쪽으로 이동
-# character.clip_draw(left, bottom, animation_frame_wANDh[0][0],
-#                     animation_frame_wANDh[0][1], 400, 300)
-# update_canvas()
-
 while True:
     clear_canvas()
 
     bottom = sprite_height - (2 * animation_frame_wANDh[0][1]) + 10
-    left = frame * animation_frame_wANDh[0][0] + 5
+    left = frame * animation_frame_wANDh[0][0] + 3
+
+    # 5번째 프레임부터 left 값을 추가로 +5
+    if frame >= 5:
+        left += 5
 
     character.clip_draw(left, bottom, animation_frame_wANDh[0][0],
                         animation_frame_wANDh[0][1], 410, 300)
