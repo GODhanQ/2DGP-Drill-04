7404bc2b9bdae38f81e667ff674d73d166f4e760
GODhanQ <hg.min2018@gmail.com>
2025-09-23 19:29:58 +0900

    기존 한개만 됐던 애니메이션 루프를 애니메이션 갯수만큼 for문으로 루프를 만들어 모두 실행하도록 수행

--- Lecture05_Animation/Character_runs.py
+++ Lecture05_Animation/Character_runs.py
@@ -7,10 +7,10 @@ current_animation = 0
 # 밑에서 부터 첫 번째
 animation_frame_count = [5, 5, 6, 6]
 animation_frame_wANDh = [
-    [(300, 249), (300, 249), (300, 249), (300, 249), (300, 249), (300, 249)],
-    [(), (), (), (), (), ()],
-    [(), (), (), (), ()],
-    [(), (), (), (), ()]
+    [(300, 240), (300, 240), (300, 240), (300, 240), (300, 240) ],
+    [(300, 240), (300, 240), (300, 240), (300, 240), (300, 240) ],
+    [(300, 240), (300, 240), (300, 240), (300, 240), (300, 240), (300, 240) ],
+    [(300, 240), (300, 240), (300, 240), (300, 240), (300, 240), (300, 240) ]
 ]
 sprite_width = 1800
 sprite_height = 1000
@@ -18,20 +18,23 @@ character = load_image("Samurai_Sprite_1800x1000.png")
 
 frame = 0
 while True:
-    clear_canvas()
+    for i in range(0, animation_count):
+        current_animation = i
+        for j in range(animation_frame_count[i]):
+            # 캔버스 지우기
+            clear_canvas()
 
-    # 현재 프레임의 너비와 높이 가져오기
-    width, height = animation_frame_wANDh[0][frame]
+            # 현재 프레임의 너비와 높이 가져오기
+            width, height = animation_frame_wANDh[i][j]
 
-    bottom = current_animation * (sprite_height // 4)
-    left = frame * sprite_width // 6
+            bottom = current_animation * (sprite_height // 4)
+            left = j * sprite_width // 6
 
-    character.clip_draw(left, bottom, width, height, 400, 300)
-    update_canvas()
+            character.clip_draw(left, bottom, width, height, 400, 300)
+            update_canvas()
 
-    # 프레임 업데이트 및 딜레이
-    print(frame)
-    frame = (frame + 1) % animation_frame_count[0]
-    delay(0.5)
+            # 프레임 업데이트 및 딜레이
+            print(j)
+            delay(0.5)
 
 close_canvas()
